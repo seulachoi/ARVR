4d0eafb8789010184e6dc571d4d377810bf94517
seulachoi <67857659+seulachoi@users.noreply.github.com>
2021-03-14 19:59:05 +0900

    videoclip 2개에서 in, out 구현하기
    videoclip 2개에서 in, out 구현하기

---
+++ buttonManager1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+public class buttonManager1 : MonoBehaviour
+{
+    public GameObject sphere;
+    public VideoClip clip01;
+    public VideoClip clip02;
+    public GameObject button01;
+    public GameObject button02;
+
+    public void toVideoScene()
+    {
+        SceneManager.LoadScene("360Video");
+    }
+    
+    public void goIn()
+    {
+        button01.SetActive(false);
+        button02.SetActive(true);
+        sphere.GetComponent<VideoPlayer>().clip = clip02;
+    }
+    public void goOut()
+    {
+        button01.SetActive(true);
+        button02.SetActive(false);
+        sphere.GetComponent<VideoPlayer>().clip = clip01;
+    }
+}
